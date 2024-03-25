8192cd8cb840a78d3efb6fb657a249847ecc5928
Legion204 <ri46801@gmail.com>
2024-03-25 19:44:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='font-extrabold playfairDisplay text-8xl'>Vite + React</h1>
+      <h1 className='font-extrabold  text-8xl workSans'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
